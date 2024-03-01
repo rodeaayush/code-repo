d4677ac5a1141b5170d42bbe0c56c2f2c55bd613
rodeaayush <rodeaayush2@gmail.com>
2024-03-01 16:13:45 +0530

    Your commit message here

---
+++ pull.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Commit message
+commit_message="Your commit message here"
+
+# Pull changes from the remote repository
+git pull
+
+# Add all changes to the staging area
+git add .
+
+# Commit changes with the specified commit message
+git commit -m "$commit_message"
+
+# Push changes to the remote repository
+git push
